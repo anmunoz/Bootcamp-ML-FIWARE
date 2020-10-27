7ef72ce2570a5368da76398a18cb129d6d2aad3c
anmunoz <jama87ie>
2020-10-27 05:41:27 +0100

    Update Deployment

--- deploy-scenario.py
+++ deploy-scenario.py
@@ -6,7 +6,7 @@ data = ("./data/simple_flight_delay_features.jsonl.bz2")
 distances = ("./data/origin_dest_distances.jsonl")
 
 CHECK_DATA = os.path.exists(data and distances)
-CHECK_FOLDER = os.path.isdir(MYDIR)
+CHECK_FOLDER = os.path.exists(MYDIR)
 
 if not CHECK_DATA:
 	print('Downloading the data...')
@@ -15,12 +15,13 @@ if not CHECK_DATA:
 if not CHECK_FOLDER:
     print('Start training containers')
     os.system('docker-compose -f docker-compose-training.yml up -d')
+    print('Training ML model, it can take a while')
 
-while not CHECK_FOLDER:
-    time.sleep(30)
+while not os.path.exists(MYDIR):
     print('Still training...')
+    time.sleep(30)
 
-if CHECK_FOLDER:
+if os.path.exists(MYDIR):
     os.system('docker-compose -f docker-compose-training.yml down')
     time.sleep(3)
     print('Start prediction containers')
