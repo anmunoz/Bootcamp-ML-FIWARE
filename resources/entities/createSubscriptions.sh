85ca170d07c793067ebb29ec24bff5cd7879f034
anmunoz <jama87ie>
2020-10-27 17:52:07 +0100

    Update docker compose add draco

--- resources/entities/createSubscriptions.sh
+++ resources/entities/createSubscriptions.sh
@@ -83,3 +83,36 @@ curl -v orion:1026/v2/subscriptions -s -S -H 'Content-Type: application/json' -d
   "throttling": 5
 }
 EOF
+
+curl -v orion:1026/v2/subscriptions -s -S -H 'Content-Type: application/json' -d @- <<EOF 
+{
+  "description": "A subscription to get info about ResFlightPrediction1",
+  "subject": {
+    "entities": [
+      {
+        "id": "ResFlightPrediction1",
+        "type": "flight"
+      }
+    ],
+    "condition": {
+      "attrs": [
+        "socketId",
+        "predictionId",
+        "predictionValue"
+      ]
+    }
+  },
+  "notification": {
+    "http": {
+      "url": "http://draco:5050/v2/notify"
+    },
+    "attrs": [
+      "socketId",
+      "predictionId",
+      "predictionValue"
+    ]
+  },
+  "expires": "2040-01-01T14:00:00.00Z",
+  "throttling": 5
+}
+EOF
